602fc4122b1ee0f1eba7ac6eed00ed570674f572
Przemyslaw Pajak <przemyslaw.pajak@creative-works.us>
2022-06-20 18:23:23 +0200

    Added first POST request

--- app.lua
+++ app.lua
@@ -1,10 +1,22 @@
 local lapis = require("lapis")
+local respond_to = require("lapis.application").respond_to
 local app = lapis.Application()
 app:enable('etlua')
 app.layout = require "views.layout"
 
-app:get("/", function()
+app:get("index", "/", function()
   return { render = "index" }
 end)
+
+app:match("contact", "/kontakt", respond_to(
+{
+  GET = function ()
+    return { render = "contact" }
+  end,
+  POST = function (self)
+    return { redirect_to = self:url_for("index")}
+  end
+}
+))
 
 return app
