0cf7dffb2f387292fb3f00c01af34dfebc05a888
Przemyslaw Pajak <przemyslaw.pajak@creative-works.us>
2022-07-03 21:07:59 +0200

    Formatting

--- app.lua
+++ app.lua
@@ -1,22 +1,24 @@
 local lapis = require("lapis")
 local respond_to = require("lapis.application").respond_to
 local app = lapis.Application()
-app:enable('etlua')
-app.layout = require "views.layout"
+app:enable("etlua")
+app.layout = require("views.layout")
 
 app:get("index", "/", function()
-  return { render = "index" }
+	return { render = "index" }
 end)
 
-app:match("contact", "/kontakt", respond_to(
-{
-  GET = function ()
-    return { render = "contact" }
-  end,
-  POST = function (self)
-    return { redirect_to = self:url_for("index")}
-  end
-}
-))
+app:match(
+	"contact",
+	"/kontakt",
+	respond_to({
+		GET = function()
+			return { render = "contact" }
+		end,
+		POST = function(self)
+			return { redirect_to = self:url_for("index") }
+		end,
+	})
+)
 
 return app
